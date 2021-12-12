4962fd04a6334d081e793e9f5d60bbc28bab4710
bikram-gill <68790260+bikram-gill@users.noreply.github.com>
2021-12-12 14:55:14 +0530

    Uploading first go file

---
+++ solutions/theGoProgrammingLanguage/Ch01-Ex1.1.go
@@ -0,0 +1,30 @@
+package main 
+
+import (
+    "fmt"
+    "os"
+    "strings"
+)
+
+//To test: "go run Ch01-Ex1.1.go -v -a -b testing", random test strings after file name.
+func main() {
+	//echo1
+	var s, sep string
+	for i := 0; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+	
+	//echo2
+	s2, sep2 := "", ""
+	for _,arg := range os.Args[0:] {
+		s2 += sep2 + arg
+		sep2 = " "
+	}
+	fmt.Println(s2)
+	
+	//echo3
+    fmt.Println(strings.Join(os.Args, " "))	
+}
+
