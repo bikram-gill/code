aa1320da0327419d5f774ecd7c53221fbe2a6c93
bikram-gill <68790260+bikram-gill@users.noreply.github.com>
2021-12-16 23:36:48 +0530

    Split spellathon and allthon functions

--- games/spellathon/python/spellathon.py
+++ games/spellathon/python/spellathon.py
@@ -30,145 +30,34 @@ import sys
 CONSTANTS
 '''
 DATA_FILE_PATH = './games/spellathon/data'     #All data files are stored in this folder
-TEMP_DATA_FILE_PATH = DATA_FILE_PATH + '/temp' #All temp files are here
+TEMP_FOLDER = 'temp' #All temp files are here
 DATA_FILE_WORDS = 'words.txt'           #First input file with word list
 DATA_FILE_LONGWORDS = 'long_words.txt'  #Derived from input file, words.txt
 FOLDER_SEPARATOR = '/'                  #Folder path separator used in Windows - use system var
 TEMP_FILE_PREFIX = 'temp_file'          #Common prefix used for generating temp files
 
 ALLTHON_WORD_LENGTH_MIN = 7             #Minimum length of words used in allthon
+SPELLATHON_WORD_LENGTH_MIN = 4          #Minimum length of words used in spellathon
+SPELLATHON_WORD_LENGTH_MAX = 7          #Maximum length of words used in spellathon
 LEWAND_ALPHABETS = 'etaoinshrdlcumwfgypbvkjxqz' #average frequency of word usage
 
 '''
-------------------------------------------------------------------------
-Variation of Spellathon: Allthon
-
-This section contains code for finding words that contain all 7 letters.
-Other letters can also occur in the words. 
-------------------------------------------------------------------------
+Common functions
 '''
 
-'''
-parseWordListAndSaveLongWords - following function parses word list file,
- and generates a new file containing words of length 7 or more. Used for 
- finding words which contain all 7 letters.
- File parsed is './../data/words.txt' and file generated is 
-'./../data/long_words.txt'
-'''
-def parseWordListAndSaveLongWords(file_name_with_path):
-    try:
-        words = open(file_name_with_path, 'r')
-    except FileNotFoundError:
-        print('File not found. A list of words, with a word in each line, should exist: ' + file_name_with_path)
-        return
-    
-    
-    long_words = open(DATA_FILE_PATH + FOLDER_SEPARATOR + 'long_words.txt', 'w')
-        
-    readLines = 0
-    writtenLines = 0
-    
-    while True:
-        line = words.readline()
-        
-        if not line:
-            break
-        readLines += 1
-        final_line = line.strip()
-        if len(final_line) >= ALLTHON_WORD_LENGTH_MIN:
-            long_words.writelines( final_line + '\n')
-            writtenLines += 1
-    
-    print("Lines read: {}, Lines written: {}".format( readLines, writtenLines))
-    
-    words.close()
-    long_words.close()
-
-'''
-findWordsWithAllAlphabetsSimpleSearch takes a string with 7 letters with 
-which words need to be created.
-
-This function implements a simple logic, and other version will try to
- implement an optimized solution and/or use regex.
- 
-NOTE: This function does not exactly solve spellathon, but finds words 
-which use all 7 letters. In Spellathon, only the given 7 letters have 
-to be used. To be moved to different file.
-
-Assumptions:
--First alphabet in the string is the central letter, which needs to 
-occur in each word.
--This function assumes that a file exists, ./../data/words.txt, 
-which contains all the words that should be used for solving this puzzle.
--Code assumes a temp folder exists in path './../data/temp', and it 
-is used for storing temporary files
--This version assumes all chars are unique, and future version will 
-handle duplicate chars.
--Lewand list of alphabets for most common to least common alphabets 
-in appearance is used, to shortlist the word list faster: 
-etaoinshrdlcumwfgypbvkjxqz
-( https://en.wikipedia.org/wiki/Letter_frequency )
-'''
-def findWordsWithAllAlphabetsSimpleSearch(alphabetList):
-    
-    #Create file with all words longer than length 7
-    parseWordListAndSaveLongWords(DATA_FILE_PATH + FOLDER_SEPARATOR + 'words.txt')
-    
-    #initialize temp file names
-    temp_file_names = [TEMP_FILE_PREFIX +str(x) for x in range(7)] 
-    
-    #TODO: Following code is really for spellathon, and needs to move there. Allthon searches for all words
-    parseWordListAndSaveWords(DATA_FILE_PATH + FOLDER_SEPARATOR + 'long_words.txt', 
-                              temp_file_names[0], alphabetList[0])
-    
-    #sort the list of alphabets based on occurrence to filter words faster
-    #filtering least used words first
-    sorted_alphabets = sortSearchLettersAsPerFrequency(alphabetList[1:])
-    
-    previous_temp_file = temp_file_names[0]
-    
-    for char_to_search, temp_file in zip(sorted_alphabets, temp_file_names[1:]):
-        parseWordListAndSaveWords(DATA_FILE_PATH + FOLDER_SEPARATOR + 'temp' 
-                                    + FOLDER_SEPARATOR 
-                                    + previous_temp_file, temp_file, char_to_search)
-        previous_temp_file = temp_file
-    
-    #Read final file, extract the shortlisted words and print them
-    final_word_file = open(DATA_FILE_PATH + FOLDER_SEPARATOR + 'temp' 
-                                    + FOLDER_SEPARATOR + temp_file_names[-1], 'r')
-    
-    final_words = []
-    final_word_count = 0
-    
-    while True:
-        line = final_word_file.readline()
-        
-        if not line:
-            break
-        final_word_count += 1
-        final_line = line.strip()
-        final_words.append(final_line)
-    
-    final_word_file.close()
-    
-    print("Words with: {}, Count: {}".format( alphabetList, final_word_count))
-    
-    print(final_words)
-    
-    #TO DO: delete temp files
-    
-    return final_words
-     
-    
 '''
 parseWordListAndSaveWords - checks each word in input file for occurrence
- of a word, and saves these words in a new file.
+ of a character, and saves these words in a new file.
+ 
+file_name_with_path - file name with words to filter
+temp_file_name - file name of file created with filtered words
+char_to_search - character to search, in each word, in the input file
 '''
 def parseWordListAndSaveWords(file_name_with_path, temp_file_name, 
                               char_to_search):
     words = open(file_name_with_path, 'r')
     
-    char_words = open(DATA_FILE_PATH + FOLDER_SEPARATOR + 'temp' + FOLDER_SEPARATOR + temp_file_name, 'w')
+    char_words = open(DATA_FILE_PATH + FOLDER_SEPARATOR + TEMP_FOLDER + FOLDER_SEPARATOR + temp_file_name, 'w')
         
     readLines = 0
     writtenLines = 0
@@ -201,6 +90,178 @@ def sortSearchLettersAsPerFrequency(alphabetList):
     return returnList[::-1]
 
 '''
+parseWordListAndSaveLongWords - following function parses word list file,
+ and generates a new file containing words of length 7 or more. Used for 
+ finding words which contain all 7 letters.
+ File parsed is './../data/words.txt' and file generated is 
+'./../data/long_words.txt'
+
+file_name_with_path - input file to parse for words, one word in each line
+word_length_to_filter - words of length longer that this number are filtered
+into a new file
+'''
+def parseWordListAndSaveLongWords(file_name_with_path, word_length_to_filter):
+    try:
+        words = open(file_name_with_path, 'r')
+    except FileNotFoundError:
+        print('File not found. A list of words, with a word in each line, should exist: ' + file_name_with_path)
+        return
+    
+    #words longer than word_length_to_filter are saved in following file
+    long_words = open(DATA_FILE_PATH + FOLDER_SEPARATOR + TEMP_FOLDER + FOLDER_SEPARATOR + DATA_FILE_LONGWORDS, 'w')
+        
+    readLines = 0
+    writtenLines = 0
+    
+    #loop through the input file words, and save long words in new file, long_words.txt
+    while True:
+        line = words.readline()
+        
+        if not line:
+            break
+        readLines += 1
+        final_line = line.strip()
+        if len(final_line) >= word_length_to_filter:
+            long_words.writelines( final_line + '\n')
+            writtenLines += 1
+    
+    print("Lines read: {}, Lines written: {}".format( readLines, writtenLines))
+    
+    words.close()
+    long_words.close()
+
+
+'''
+parseWordListOfGivenLength - following function parses word list file,
+ and generates a new file containing words of length 4 to 7.
+ 
+ File parsed is './../data/words.txt' and file generated is './../data/given_words.txt'
+
+file_name_with_path - input file to parse for words, one word in each line
+min_length_to_filter, max_length_to_filter - words of length between min and max are filtered
+into a new file
+'''
+def parseWordListOfGivenLength(file_name_with_path, min_length_to_filter, max_length_to_filter):
+    try:
+        words = open(file_name_with_path, 'r')
+    except FileNotFoundError:
+        print('File not found. A list of words, with a word in each line, should exist: ' + file_name_with_path)
+        return
+    
+    #words longer than word_length_to_filter are saved in following file
+    long_words = open(DATA_FILE_PATH + FOLDER_SEPARATOR + TEMP_FOLDER + FOLDER_SEPARATOR + DATA_FILE_LONGWORDS, 'w')
+        
+    readLines = 0
+    writtenLines = 0
+    
+    #loop through the input file words, and save long words in new file, long_words.txt
+    while True:
+        line = words.readline()
+        
+        if not line:
+            break
+        readLines += 1
+        final_line = line.strip()
+        if len(final_line) >= min_length_to_filter and len(final_line) <= max_length_to_filter:
+            long_words.writelines( final_line + '\n')
+            writtenLines += 1
+    
+    print("Lines read: {}, Lines written: {}".format( readLines, writtenLines))
+    
+    words.close()
+    long_words.close()
+
+
+'''
+------------------------------------------------------------------------
+Variation of Spellathon: Allthon
+
+This section contains code for finding words that contain all 7 letters.
+Other letters can also occur in the words. 
+------------------------------------------------------------------------
+'''
+
+'''
+findWordsWithAllAlphabetsSimpleSearch takes a string with 7 letters with 
+which words need to be created.
+
+This function implements a simple logic, and other version will try to
+ implement an optimized solution and/or use regex.
+ 
+NOTE: This function does not exactly solve spellathon, but finds words 
+which use all 7 letters. In Spellathon, only the given 7 letters have 
+to be used. To be moved to different file.
+
+TODO: Modify code to process alphabet list other than 7
+
+Assumptions:
+-First alphabet in the string is the central letter, which needs to 
+occur in each word.
+-This function assumes that a file exists, ./../data/words.txt, 
+which contains all the words that should be used for solving this puzzle.
+-Code assumes a temp folder exists in path './../data/temp', and it 
+is used for storing temporary files
+-This version assumes all chars are unique, and future version will 
+handle duplicate chars.
+-Lewand list of alphabets for most common to least common alphabets 
+in appearance is used, to shortlist the word list faster: 
+etaoinshrdlcumwfgypbvkjxqz
+( https://en.wikipedia.org/wiki/Letter_frequency )
+'''
+def findWordsWithAllAlphabetsSimpleSearch(alphabetList):
+    
+    #Create file with all words longer than length 7 (ALLTHON_WORD_LENGTH_MIN)
+    parseWordListAndSaveLongWords(DATA_FILE_PATH + FOLDER_SEPARATOR + DATA_FILE_WORDS,ALLTHON_WORD_LENGTH_MIN)
+    
+    #initialize temp file names
+    temp_file_names = [TEMP_FILE_PREFIX +str(x) for x in range(7)] 
+    
+    #TODO: Following code is really for spellathon, and needs to move there. Allthon searches for all words
+    #parseWordListAndSaveWords(DATA_FILE_PATH + FOLDER_SEPARATOR + DATA_FILE_LONGWORDS, 
+    #                          temp_file_names[0], alphabetList[0])
+    
+    #sort the list of alphabets based on occurrence to filter words faster
+    #filtering least used words first
+    sorted_alphabets = sortSearchLettersAsPerFrequency(alphabetList)
+    
+    #start with input file
+    previous_temp_file = DATA_FILE_LONGWORDS
+    
+    for char_to_search, temp_file in zip(sorted_alphabets, temp_file_names):
+        parseWordListAndSaveWords(DATA_FILE_PATH + FOLDER_SEPARATOR + TEMP_FOLDER 
+                                    + FOLDER_SEPARATOR 
+                                    + previous_temp_file, temp_file, char_to_search)
+        previous_temp_file = temp_file
+    
+    #Read final file, extract the shortlisted words and print them
+    final_word_file = open(DATA_FILE_PATH + FOLDER_SEPARATOR + TEMP_FOLDER 
+                                    + FOLDER_SEPARATOR + temp_file_names[-1], 'r')
+    
+    final_words = []
+    final_word_count = 0
+    
+    while True:
+        line = final_word_file.readline()
+        
+        if not line:
+            break
+        final_word_count += 1
+        final_line = line.strip()
+        final_words.append(final_line)
+    
+    final_word_file.close()
+    
+    print("Words with: {}, Count: {}".format( alphabetList, final_word_count))
+    
+    print(final_words)
+    
+    #TO DO: delete temp files
+    
+    return final_words
+     
+    
+
+'''
 ------------------------------------------------------------------------
 Spellathon
 
@@ -214,14 +275,55 @@ letters (only 1 combination of all 7 letters exists).
 ------------------------------------------------------------------------
 '''
 
-#TO DO
+'''
+findWordsForSpellathonSimpleSearch takes a string with 7 letters with 
+which words need to be created.
+
+This function implements a simple logic, and other version will try to
+ implement an optimized solution and/or use regex.
+ 
+Assumptions:
+-First alphabet in the string is the central letter, which needs to 
+occur in each word.
+-This function assumes that a file exists, ./../data/words.txt, 
+which contains all the words that should be used for solving this puzzle.
+-Code assumes a temp folder exists in path './../data/temp', and it 
+is used for storing temporary files
+-This version assumes all chars are unique, and future version will 
+handle duplicate chars.
+-Lewand list of alphabets for most common to least common alphabets 
+in appearance is used, to shortlist the word list faster: 
+etaoinshrdlcumwfgypbvkjxqz
+( https://en.wikipedia.org/wiki/Letter_frequency )
+'''
+def findWordsForSpellathonSimpleSearch(alphabetList):
+    '''
+    1. Find words of length between 4 and 7, both inclusive, because in spellathon
+    we can only use the given letters
+    2. Find all combinations of letters which are of length 4 to 7
+    3. Find words which are made up of only letters in any of the above combinations
+    '''
+    #Create file with all words between length 4-7
+    parseWordListOfGivenLength(DATA_FILE_PATH + FOLDER_SEPARATOR + DATA_FILE_WORDS, SPELLATHON_WORD_LENGTH_MIN, SPELLATHON_WORD_LENGTH_MAX)
+
+    #TODO
+
+    pass
+
+
 
 if __name__ == '__main__':
     #print(os.getcwd())
-    #'''
+    '''
     try:
-        findWordsWithAllAlphabetsSimpleSearch('petouzv')
+        findWordsWithAllAlphabetsSimpleSearch('petiuzv')
     except:
         print(sys.exc_info()[0])
     print('end')
-    #'''
+    '''
+    try:
+        findWordsForSpellathonSimpleSearch('petiuzv')
+    except:
+        print(sys.exc_info()[0])
+    print('end')
+    
