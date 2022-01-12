947bba368693ab6d0eee56a9f4fb2f32313a886d
bikram-gill <68790260+bikram-gill@users.noreply.github.com>
2022-01-12 23:27:02 +0530

    Updated factorial function

--- solutions/projectEuler/python/P0015-LatticePaths.py
+++ solutions/projectEuler/python/P0015-LatticePaths.py
@@ -3,23 +3,14 @@ Number of paths (between 2 diagonal corners) along edges of a NxN grid,
 only moving right or down = (2 * N)! / (N! * N!)
 '''
 from datetime import datetime
-
-def factorial(N):
-    factorialArray = [0 for x in range(N+1)]
-
-    factorialArray[1] = 1
-
-    for i in range(2, N+1):
-        factorialArray[i] = factorialArray[i-1] * i
-
-    return factorialArray[N]
+import math
 
 def numberOfLatticePaths(N):
     
-    Numerator = factorial(2 * N)
-    Denominator = factorial (N)
+    Numerator = math.factorial(2 * N)
+    Denominator = math.factorial(N)
 
-    return int(Numerator / (Denominator * Denominator))
+    return int( Numerator // (Denominator * Denominator))
 
 if __name__ == '__main__':
     N = int( input('Input number of edges (E.g. 20): ') )
