db70b7a3c8640328cda905f4ea62d5773a73861e
bikram-gill <68790260+bikram-gill@users.noreply.github.com>
2022-01-14 16:09:18 +0530

    Updated faster version solution for problem 21

--- solutions/projectEuler/python/P0021-AmicableNumbers.py
+++ solutions/projectEuler/python/P0021-AmicableNumbers.py
@@ -4,6 +4,7 @@ Find sum of all amicable number less than N.
 
 from datetime import datetime
 
+#This method can take upto 30+ seconds for N > 30000
 def sumOfAmicableNumbers(N):
       
     amicableNumbers = set()
@@ -19,12 +20,47 @@ def sumOfAmicableNumbers(N):
       
     return sum(amicableNumbers)
 
+#A faster version and can process N == 1M in less than 10 seconds
+def sumOfAmicableNumbersFast(N):
+
+    #random number found by testing, for N == 1M, times should be atleast 4
+    times = 4
+
+     #1 is a divisor for all integers
+     #Initialize 2D array to store divisors of number at respective indexes  
+    divisors = [ [1] for i in range(times*N+1)]
+
+    for number in range(2,times*N+1):
+        for multiple in range(number*2, N+1, number):
+            divisors[multiple].append( number )
+
+    #Dictionary to store sum of divisors for each number
+    sumOfDivisors = dict()
+    for number in range(1,times*N+1):
+        sumOfDivisors[number] = sum(divisors[number])
+
+    amicableNumbers = set()
+
+    for number in sumOfDivisors:
+
+        if number > N:
+            break
+
+        sumOfDivisor = sumOfDivisors[number]
+        
+        if number == sumOfDivisors[sumOfDivisor] and sumOfDivisor != number:
+            amicableNumbers.add(number)
+            amicableNumbers.add(sumOfDivisor)
+
+    return sum(amicableNumbers)
+
 if __name__ == '__main__':
     N = int( input('Input upper bound for finding sum of amicable numbers (E.g. 10000): ') )
 
     start = datetime.now()
 
-    print('Answer: ', str( sumOfAmicableNumbers(N) ) )
+    #print('Answer: ', str( sumOfAmicableNumbers(N) ) )
+    print('Answer: ', str( sumOfAmicableNumbersFast(N) ) )
 
     end = datetime.now()
 
